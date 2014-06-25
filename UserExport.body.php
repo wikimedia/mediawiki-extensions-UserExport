0f20c0d5521908c8415c0e76012e5dcc3a065fae
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-06-25 16:39:58 +0200

    Update and rename userexport.body.php to UserExport.body.php
    Add code from https://www.mediawiki.org/wiki/Extension:UserExport

--- UserExport.body.php
+++ UserExport.body.php
@@ -1,8 +1,8 @@
 <?php
-/** \file
-* \brief Contains code for the UserExport Class (extends SpecialPage).
-*/
-
+/**
+ * Main file for the UserExport extension.
+ */
+ 
 ///Special page class for the User Export extension
 /**
  * Special page that allows sysops to export the username and
@@ -15,19 +15,17 @@ class UserExport extends SpecialPage {
 	function __construct() {
 		parent::__construct( 'UserExport', 'userexport' );
 	}
-
+ 
 	function execute( $par ) {
 		global $wgRequest, $wgOut, $wgUser;
-
-		wfLoadExtensionMessages( 'UserExport' );
-
+ 
 		$this->setHeaders();
-
+ 
 		if ( !$wgUser->isAllowed( 'userexport' ) ) {
 			$wgOut->permissionRequired( 'userexport' );
 			return;
 		}
-
+ 
         if ( $wgRequest->getText( 'exportusers' ) ) {
             if ( !$wgUser->matchEditToken( $wgRequest->getVal( 'token' ) ) ) {
                 // bad edit token
@@ -36,40 +34,40 @@ class UserExport extends SpecialPage {
                 $this->exportUsers();
             }
         }
-
+ 
         $wgOut->addHTML(
             Xml::openElement('p') .
             wfMsg( 'userexport-description' ) .
             Xml::closeElement('p') . 
             Xml::openElement( 'form', array( 'method' => 'post', 'action' => $this->getTitle()->getLocalUrl(), 'id' => 'userexportform' ) ) .
             Xml::submitButton( wfMsg( 'userexport-submit' ) ) .
-			Xml::hidden( 'token', $wgUser->editToken() ) .
-	    	Xml::hidden( 'exportusers', true ) .
+			Html::hidden( 'token', $wgUser->editToken() ) .
+	    	Html::hidden( 'exportusers', true ) .
 		    Xml::closeElement( 'form' ) . "\n"
 		);
 	}
-
-	/**
+ 
+    /**
      * Function to query the database and generate the CVS file
      *
-	 * @return Always returns true - throws exceptions on failure.
-	 */
+     * @return Always returns true - throws exceptions on failure.
+     */
     private function exportUsers()
     {
         $filePath = tempnam(sys_get_temp_dir(), '');
         $file = fopen($filePath, 'w');
-
+ 
         $db = wfGetDB( DB_MASTER );
         $users = $db->select('user', array('user_name', 'user_email'));
-        
+ 
         fputcsv($file, array('login', 'email'));
-
+ 
         while ( $user = $db->fetchObject( $users ) ) {
             fputcsv($file, array($user->user_name, $user->user_email));
         }
-
+ 
         fclose($file);
-
+ 
         header("Pragma:  no-cache");
         header("Expires: 0");
         header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
@@ -80,7 +78,7 @@ class UserExport extends SpecialPage {
         header("Content-Disposition: attachment; filename=\"mediawiki_users.csv\"");
         header("Content-Length: " . filesize($filePath));  
         header("Accept-Ranges: bytes");  
-
+ 
         readfile($filePath);
         unlink($filePath);
         die;
