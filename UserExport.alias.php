f94100b7b64defc960061e62c16b0f65e7013f0c
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-06-25 16:41:52 +0200

    Update and rename userexport.alias.php to UserExport.alias.php
    Add code from https://www.mediawiki.org/wiki/Extension:UserExport

---
+++ UserExport.alias.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Internationalisation file for the special page aliases of the UserExport extension.
+ *
+ * @author RodrigoSampaioPrimo
+ *
+ * @licence GNU General Public Licence 2.0 or later
+ */
+ 
+$aliases = array();
+ 
+/** English
+ * @author RodrigoSampaioPrimo
+ */
+$aliases['en'] = array(
+	'userexport' => array( 'userexport' ),
+);
+ 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$aliases['de'] = array(
+	'userexport' => array( 'Benutzerdatenexport' ),
+);
