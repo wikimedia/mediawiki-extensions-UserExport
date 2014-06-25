d4faeed78035ac46b07a6beca8fdc4fbcd004950
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-06-25 16:40:51 +0200

    Update and rename userexport.i18n.php to UserExport.i18n.php
    Add code from https://www.mediawiki.org/wiki/Extension:UserExport

---
+++ UserExport.i18n.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Internationalisation file for the system messages of the UserExport extension.
+ *
+ * @author RodrigoSampaioPrimo
+ *
+ * @licence GNU General Public Licence 2.0 or later
+ */
+ 
+$messages = array();
+ 
+/** English
+ * @author RodrigoSampaioPrimo
+ */
+$messages['en'] = array(
+    'userexport-desc'         		 => 'Allows to export the user names and the respective email addresses to a CSV file',
+    'userexport'                     => 'User Export',
+    'userexport-submit'              => 'Export users to CSV file',
+    'userexport-badtoken' 		     => 'Invalid edit token',
+    'userexport-description'         => 'This is a very simple extension to export Mediawiki users to a CSV file.<br />It only export the user name and user email, but it can be easily extended to support more user fields.',
+    'userexport-logpage'         	=> 'User export log',
+    'userexport-logpagetext'          => 'Below is a list of all user exports which have been performed',
+);
+ 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+    'userexport-desc'        		 => 'Ergänzt eine [[Spezial:UserExport|Spezialseite]] zum Exportieren von Benutzerdaten im CSV-Format',
+    'userexport'                     => 'Benutzerdatenexport',
+    'userexport-submit'              => 'Benutzerdaten exportieren',
+    'userexport-badtoken' 		     => 'Der Bearbeitungstoken ist ungültig.',
+    'userexport-description'         => 'Dies ist eine einfache Programmerweiterung mit der man die Benutzerdaten (Name und E-Mail-Adresse) der auf dem Wiki registrierten Benutzer in eine CSV-Datei exportieren kann.',
+    'userexport-logpage'         	=> 'Benutzerdatenexport-Logbuch',
+    'userexport-logpagetext'          => 'In diesem Logbuch werden die Exporte von Benutzerdaten protokolliert.',
+);
