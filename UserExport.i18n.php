eae81661b0851912a870d27feab25ea2cede675d
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-11-14 19:33:09 +0100

    Update UserExport.i18n.php
    Remove dead code connected to logging actions, follow up to https://github.com/kghbln/UserExport/commit/1eb2c3ccb6d79fe7a8786d84ded723336a79c185

--- UserExport.i18n.php
+++ UserExport.i18n.php
@@ -2,7 +2,7 @@
 /**
  * Internationalisation file for the system messages of the UserExport extension.
  *
- * @author RodrigoSampaioPrimo
+ * @author Rodrigoprimo
  *
  * @licence GNU General Public Licence 2.0 or later
  */
@@ -10,16 +10,14 @@
 $messages = array();
  
 /** English
- * @author RodrigoSampaioPrimo
+ * @author Rodrigoprimo
  */
 $messages['en'] = array(
     'userexport-desc'         		 => 'Allows to export the user names and the respective email addresses to a CSV file',
     'userexport'                     => 'User Export',
     'userexport-submit'              => 'Export users to CSV file',
     'userexport-badtoken' 		     => 'Invalid edit token',
-    'userexport-description'         => 'This is a very simple extension to export Mediawiki users to a CSV file.<br />It only export the user name and user email, but it can be easily extended to support more user fields.',
-    'userexport-logpage'         	=> 'User export log',
-    'userexport-logpagetext'          => 'Below is a list of all user exports which have been performed',
+    'userexport-description'         => 'This is a very simple extension to export Mediawiki users to a CSV file.<br />It only export the user name and user email, but it can be easily extended to support more user fields.'
 );
  
 /** German (Deutsch)
@@ -30,7 +28,5 @@ $messages['de'] = array(
     'userexport'                     => 'Benutzerdatenexport',
     'userexport-submit'              => 'Benutzerdaten exportieren',
     'userexport-badtoken' 		     => 'Der Bearbeitungstoken ist ungültig.',
-    'userexport-description'         => 'Dies ist eine einfache Programmerweiterung mit der man die Benutzerdaten (Name und E-Mail-Adresse) der auf dem Wiki registrierten Benutzer in eine CSV-Datei exportieren kann.',
-    'userexport-logpage'         	=> 'Benutzerdatenexport-Logbuch',
-    'userexport-logpagetext'          => 'In diesem Logbuch werden die Exporte von Benutzerdaten protokolliert.',
+    'userexport-description'         => 'Dies ist eine einfache Programmerweiterung mit der man die Benutzerdaten (Name und E-Mail-Adresse) der auf dem Wiki registrierten Benutzer in eine CSV-Datei exportieren kann.'
 );
