d200dd4c3e13a952746a7ec577dd109c0b070ad2
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-11-14 19:51:22 +0100

    Update UserExport.i18n.php
    Improve system messages

--- UserExport.i18n.php
+++ UserExport.i18n.php
@@ -13,20 +13,20 @@ $messages = array();
  * @author Rodrigoprimo
  */
 $messages['en'] = array(
-    'userexport-desc'         		 => 'Allows to export the user names and the respective email addresses to a CSV file',
-    'userexport'                     => 'User Export',
-    'userexport-submit'              => 'Export users to CSV file',
-    'userexport-badtoken' 		     => 'Invalid edit token',
-    'userexport-description'         => 'This is a very simple extension to export Mediawiki users to a CSV file.<br />It only export the user name and user email, but it can be easily extended to support more user fields.'
+    'userexport-desc'        => 'Allows to export user data to a CSV file',
+    'userexport'             => 'User Export',
+    'userexport-submit'      => 'Export users to CSV file',
+    'userexport-badtoken'    => 'Invalid edit token',
+    'userexport-description' => 'This page allows to export the user data (user name and user e-mail) of all users registered on this wiki to a CSV-file.'
 );
  
 /** German (Deutsch)
  * @author Kghbln
  */
 $messages['de'] = array(
-    'userexport-desc'        		 => 'Ergänzt eine [[Spezial:UserExport|Spezialseite]] zum Exportieren von Benutzerdaten im CSV-Format',
-    'userexport'                     => 'Benutzerdatenexport',
-    'userexport-submit'              => 'Benutzerdaten exportieren',
-    'userexport-badtoken' 		     => 'Der Bearbeitungstoken ist ungültig.',
-    'userexport-description'         => 'Dies ist eine einfache Programmerweiterung mit der man die Benutzerdaten (Name und E-Mail-Adresse) der auf dem Wiki registrierten Benutzer in eine CSV-Datei exportieren kann.'
+    'userexport-desc'        => 'Ermöglicht den Export von Benutzerdaten als CSV-Datei',
+    'userexport'             => 'Benutzerdatenexport',
+    'userexport-submit'      => 'Benutzerdaten exportieren',
+    'userexport-badtoken' 		 => 'Der Bearbeitungstoken ist ungültig.',
+    'userexport-description' => 'Diese Seite ermöglicht den Export die Benutzerdaten (Benutzername und E-Mail-Adresse) der auf dem Wiki registrierten Benutzer in eine CSV-Datei.'
 );
