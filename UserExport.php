c0385dd339b0210dbe514c1f36bdc7376b8ce64d
Karsten Hoffmeyer <mediawiki@kghoffmeyer.de>
2014-11-14 21:27:29 +0100

    Update UserExport.php
    * Update and improve extensions credits
    * Expand file documentation

--- UserExport.php
+++ UserExport.php
@@ -1,30 +1,44 @@
 <?php
 /**
- * Initialization file for the UserExport extension.
+ * This is the UserExport extension to MediaWiki that allows to export user data to a CSV file.
  *
- * @link https://www.mediawiki.org/wiki/Extension:UserExport Documentation
- * @link https://www.mediawiki.org/wiki/Extension_talk:UserExport Support
- *
+ * @file
  * @ingroup Extensions
+ * @package MediaWiki
  *
- * @licence http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- * @author Rodrigo Sampaio Primo (RodrigoSampaioPrimo)
+ * @version 1.1.0 2014-11-14
+ *
+ * @links https://github.com/kghbln/UserExport/blob/master/README.md Documentation
+ * @links https://www.mediawiki.org/wiki/Extension_talk:UserExport Support
+ * @links https://github.com/kghbln/UserExport/issues Bug tracker
+ * @links https://github.com/kghbln/UserExport Source code
+ *
+ * @copyright Copyright (c) 2009
+ * @author Rodrigo Sampaio Primo (RodrigoSampaioPrimo) <rodrigosprimo@gmail.com>
+ * @author Karsten Hoffmeyer (Kghbln) <kontakt@wikihoster.net>
+ *
+ * @license https://www.gnu.org/licenses/gpl-2.0 GNU General Public License 2.0 or later
  */
- 
+
 // Ensure that the script cannot be executed outside of MediaWiki.
 if ( !defined( 'MEDIAWIKI' ) ) {
     die( 'This is an extension to the MediaWiki package and cannot be run standalone.' );
 }
- 
+
+// Display extension properties on MediaWiki.
 $wgExtensionCredits['specialpage'][] = array(
-    'path'           => __FILE__,
-    'name'           => 'User Export',
-    'version'        => '1.0.1',
-    'url'            => 'https://www.mediawiki.org/wiki/Extension:UserExport',
-    'author'         => 'Rodrigo Sampaio Primo',
+    'path' => __FILE__,
+    'name' => 'User Export',
+    'author' => array(
+        'Rodrigo Sampaio Primo',
+        'Karsten Hoffmeyer'
+        ),
+    'url' => 'https://www.mediawiki.org/wiki/Extension:UserExport',
     'descriptionmsg' => 'userexport-desc',
-);
- 
+    'version' => '1.1.0',
+    'license-name' => 'GPL-2.0+'
+    );
+
 // Register extension class.
 $wgAutoloadClasses['UserExport'] = __DIR__ . '/UserExport.body.php';
 
