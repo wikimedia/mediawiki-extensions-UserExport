0430de3cd93c6073219651fdc1e6c1724351ff7b
Reedy <reedy@wikimedia.org>
2016-01-02 15:30:37 +0000

    Throw PermissionsError exception directly
    Change-Id: I63f7cd62fc3dab89f90bd3712d39dc798261a5a9

--- UserExport.body.php
+++ UserExport.body.php
@@ -22,65 +22,64 @@ class UserExport extends SpecialPage {
 		$this->setHeaders();
 
 		if ( !$wgUser->isAllowed( 'userexport' ) ) {
-			$wgOut->permissionRequired( 'userexport' );
-			return;
+			throw new PermissionsError( 'userexport' );
 		}
 
-        if ( $wgRequest->getText( 'exportusers' ) ) {
-            if ( !$wgUser->matchEditToken( $wgRequest->getVal( 'token' ) ) ) {
-                // bad edit token
-                $wgOut->addHtml( "<span style=\"color: red;\">" . wfMessage( 'userexport-badtoken' )->escaped() . "</span><br />\n" );
-            } else {
-                $this->exportUsers();
-            }
-        }
+		if ( $wgRequest->getText( 'exportusers' ) ) {
+			if ( !$wgUser->matchEditToken( $wgRequest->getVal( 'token' ) ) ) {
+				// bad edit token
+				$wgOut->addHtml( "<span style=\"color: red;\">" . wfMessage( 'userexport-badtoken' )->escaped() . "</span><br />\n" );
+			} else {
+				$this->exportUsers();
+			}
+		}
 
-        $wgOut->addHTML(
-            Xml::openElement( 'p' ) .
-            wfMessage( 'userexport-description' )->text() .
-            Xml::closeElement( 'p' ) .
-            Xml::openElement( 'form', array( 'method' => 'post', 'action' => $this->getTitle()->getLocalUrl(), 'id' => 'userexportform' ) ) .
-            Xml::submitButton( wfMessage( 'userexport-submit' )->text() ) .
+		$wgOut->addHTML(
+			Xml::openElement( 'p' ) .
+			wfMessage( 'userexport-description' )->text() .
+			Xml::closeElement( 'p' ) .
+			Xml::openElement( 'form', array( 'method' => 'post', 'action' => $this->getTitle()->getLocalUrl(), 'id' => 'userexportform' ) ) .
+			Xml::submitButton( wfMessage( 'userexport-submit' )->text() ) .
 			Html::hidden( 'token', $wgUser->getEditToken() ) .
-	    	Html::hidden( 'exportusers', true ) .
-		    Xml::closeElement( 'form' ) . "\n"
+			Html::hidden( 'exportusers', true ) .
+			Xml::closeElement( 'form' ) . "\n"
 		);
 	}
 
-    /**
-     * Function to query the database and generate the CVS file
-     *
-     * @return Always returns true - throws exceptions on failure.
-     */
-    private function exportUsers()
-    {
-        $filePath = tempnam( sys_get_temp_dir(), '' );
-        $file = fopen( $filePath, 'w' );
+	/**
+	 * Function to query the database and generate the CVS file
+	 *
+	 * @return bool Always returns true - throws exceptions on failure.
+	 */
+	private function exportUsers()
+	{
+		$filePath = tempnam( sys_get_temp_dir(), '' );
+		$file = fopen( $filePath, 'w' );
 
-        $db = wfGetDB( DB_MASTER );
-        $users = $db->select( 'user', array( 'user_name', 'user_email' ) );
+		$db = wfGetDB( DB_MASTER );
+		$users = $db->select( 'user', array( 'user_name', 'user_email' ) );
 
-        fputcsv( $file, array( 'login', 'email' ) );
+		fputcsv( $file, array( 'login', 'email' ) );
 
-        while ( $user = $db->fetchObject( $users ) ) {
-            fputcsv( $file, array( $user->user_name, $user->user_email ) );
-        }
+		while ( $user = $db->fetchObject( $users ) ) {
+			fputcsv( $file, array( $user->user_name, $user->user_email ) );
+		}
 
-        fclose( $file );
+		fclose( $file );
 
-        header( "Pragma:  no-cache" );
-        header( "Expires: 0" );
-        header( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
-        header( "Cache-Control: public" );
-        header( "Content-Description: File Transfer" );
-        header( "Content-type: text/csv" );
-        header( "Content-Transfer-Encoding: binary" );
-        header( "Content-Disposition: attachment; filename=\"mediawiki_users.csv\"" );
-        header( "Content-Length: " . filesize( $filePath ) );
-        header( "Accept-Ranges: bytes" );
+		header( "Pragma:  no-cache" );
+		header( "Expires: 0" );
+		header( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
+		header( "Cache-Control: public" );
+		header( "Content-Description: File Transfer" );
+		header( "Content-type: text/csv" );
+		header( "Content-Transfer-Encoding: binary" );
+		header( "Content-Disposition: attachment; filename=\"mediawiki_users.csv\"" );
+		header( "Content-Length: " . filesize( $filePath ) );
+		header( "Accept-Ranges: bytes" );
 
-        readfile( $filePath );
-        unlink( $filePath );
-        die;
-    }
+		readfile( $filePath );
+		unlink( $filePath );
+		die;
+	}
 }
